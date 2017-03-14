c10784c762c05a6adbfc516c1be379493fe9c067
Daniel Andrew Gonzalez <danielandrewgonzalez@Daniels-MacBook-Pro.local>
2017-03-14 10:43:43 -0400

    update

--- assets/javascript/trainLogic.js
+++ assets/javascript/trainLogic.js
@@ -88,18 +88,23 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
         // Get a Firebase Database ref
         var chatRef = firebase.database().ref("chat");
 
-        // Create a Firechat instance
-        var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+        // // Create a Firechat instance
+        // var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
 
-        // Set the Firechat user
-        chat.setUser(user.uid, user.displayName);
+        // // Set the Firechat user
+        // chat.setUser(user.uid, user.displayName);
       }         
 
       // use this line below, somehow, to create rooms of the quantity and names specified by the ajax. 
       // Firechat.createRoom(roomName, roomType, callback(roomId))
       // click to open chat room of button's topic name. 
       $(".button").on("click",function() {
-      Firechat.enterRoom(this.attr('id'));
+      // Create a Firechat instance
+        var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+        // Set the Firechat user
+        chat.setUser(user.uid, user.displayName);
+      // Firechat.enterRoom(this.attr('id'));
       });
 
 
